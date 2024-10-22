7ad9b3876bc0393f5a1f881aebedb17e77c1b452
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-10-22 16:33:49 -0500

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,22 @@
+require "./app"
+
+configure :development do
+  # we would also like a nicer error page in development
+  require "better_errors"
+  require "binding_of_caller"
+
+  # need this configure for better errors
+  use(BetterErrors::Middleware)
+  BetterErrors.application_root = __dir__
+  BetterErrors::Middleware.allow_ip!("0.0.0.0/0.0.0.0")
+
+  # appdev support patches
+  require "appdev_support"
+
+  AppdevSupport.config do |config|
+    # config.action_dispatch = true;
+    # config.active_record = true;
+    config.pryrc = :full
+  end
+  AppdevSupport.init
+end

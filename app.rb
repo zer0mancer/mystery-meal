81a00fa8f3152ef10647525d127e5768c508f998
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-10-22 23:45:21 +0000

    initial commit

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
+
+
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
+end
+
+
+get("/random_recipe") do
+  raw_data = HTTP.get("https://www.themealdb.com/api/json/v1/1/random.php")
+  parsed_data = JSON.parse(raw_data).fetch("meals")
+  @meal = parsed_data.at(0).fetch("strMeal")
+
+  erb(:mystery_meal)
 end
